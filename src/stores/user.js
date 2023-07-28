d910ccd2a686a88aff02958799ab4db31cb0915c
toky-sowell <emar@sowellapp.com>
2023-07-28 11:49:10 +0300

    get token in localStorage

--- src/stores/user.js
+++ src/stores/user.js
@@ -5,11 +5,9 @@ export const useUserStore = defineStore("user", {
   state: () => ({
     users: [],
     url: "https://sh-api-v1.up.railway.app/users",
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN1cGVyX2FkbWluQHNoLmNvbSIsInN1YiI6IjY0YmNlM2YzMDdmYWY0OTZlYmI0YWIyOSIsImlhdCI6MTY5MDMwOTk0MiwiZXhwIjoxNjkwNTY5MTQyfQ.Pv3x3FI8fJcBGYNkffCX4QdqLXgymVXnWBGf0IlhDvw",
     config: {
       headers: {
-        Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN1cGVyX2FkbWluQHNoLmNvbSIsInN1YiI6IjY0YmNlM2YzMDdmYWY0OTZlYmI0YWIyOSIsImlhdCI6MTY5MDMwOTk0MiwiZXhwIjoxNjkwNTY5MTQyfQ.Pv3x3FI8fJcBGYNkffCX4QdqLXgymVXnWBGf0IlhDvw"}`,
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
       },
     },
   }),
@@ -21,7 +19,7 @@ export const useUserStore = defineStore("user", {
           this.users = res.data;
         })
         .catch((error) => {
-          console.log("zezezeze", error);
+          console.log("error", error);
         });
     },
   },
