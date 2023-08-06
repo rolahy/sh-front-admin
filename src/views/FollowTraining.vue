4f91d60ede2b2fd83c980ae4dea1f5f0ff6d3de1
toky-sowell <emar@sowellapp.com>
2023-08-06 15:02:46 +0300

    amelioration page following

--- src/views/FollowTraining.vue
+++ src/views/FollowTraining.vue
@@ -10,17 +10,19 @@ const trainingStore = useTrainingStore();
 const trainings = computed(() => trainingStore.trainingInfo);
 
 const getYouTubeVideoId = (url) => {
-  // Expression régulière pour extraire l'ID de la vidéo à partir de l'URL YouTube
-  const regExp =
-    /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu.be\/)([a-zA-Z0-9_-]{11})/;
-  const match = url.match(regExp);
+  if (trainings.value.levels[0]?.videos[0].urlVideo) {
+    // Expression régulière pour extraire l'ID de la vidéo à partir de l'URL YouTube
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu.be\/)([a-zA-Z0-9_-]{11})/;
+    const match = url.match(regExp);
 
-  if (match && match[1]) {
-    return match[1];
-  } else {
-    // Gérer le cas où l'URL n'est pas valide
-    console.error("URL YouTube invalide !");
-    return null;
+    if (match && match[1]) {
+      return match[1];
+    } else {
+      // Gérer le cas où l'URL n'est pas valide
+      console.error("URL YouTube invalide !");
+      return null;
+    }
   }
 };
 
@@ -45,10 +47,14 @@ onMounted(() => {
       <section class="bg-white rounded-lg drop-shadow-lg">
         <div class="container px-6 py-2 mx-auto">
           <div
+            class="flex justify-center items-center text-gray-500 text-xl font-bold"
+          >
+            {{ trainings.title }}
+          </div>
+          <div
             class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-2 lg:grid-cols-3"
           >
             <div class="drop-shadow-lg p-4 flex-1">
-              <!-- Remplacez le code ci-dessous par l'élément vidéo pour vos vidéos -->
               <iframe
                 width="100%"
                 class="h-96"
@@ -63,17 +69,24 @@ onMounted(() => {
                 :key="level._id"
                 class="text-lg mb-2"
               >
-                <h2 class="text-lg font-bold mb-2 text-gray-800">
+                <h2 class="font-bold mb-2 text-gray-500">
                   {{ level.title }}
                 </h2>
                 <ul>
                   <li
                     v-for="video in level.videos"
                     :key="video._id"
-                    class="w-full ml-4 text-gray-600 border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50 cursor-pointer"
+                    class="w-full ml-2 text-gray-600 border-b-2 border-neutral-300 border-opacity-100 py-2 dark:border-opacity-50 cursor-pointer"
                     @click="playVIdeo(video)"
                   >
-                    {{ video.title }}
+                    <div class="flex w-full justify-between">
+                      <div>
+                        {{ video.title }}
+                      </div>
+                      <div>
+                        {{ video.duration }}
+                      </div>
+                    </div>
                   </li>
                 </ul>
               </div>
