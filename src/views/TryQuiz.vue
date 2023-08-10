52d7e30c9f2894dc0cc10ff85f6bea0d868005fe
toky-sowell <emar@sowellapp.com>
2023-08-10 22:32:47 +0300

    change design quiz

--- src/views/TryQuiz.vue
+++ src/views/TryQuiz.vue
@@ -16,21 +16,28 @@ const customElementsForm = reactive({
   <LayoutAuthenticated>
     <SectionMain>
       <!-- component -->
-      <section class="bg-white dark:bg-slate-900/50 rounded-lg drop-shadow-lg">
+      <section
+        class="bg-white text-gray-700 dark:text-white dark:bg-slate-900/50 rounded-lg drop-shadow-lg"
+      >
         <!-- {{ customElementsForm.radio }} index choix -->
         <div class="container py-2 mx-auto">
           <!-- component -->
           <!-- This is an example component -->
-          <div
-            class="flex items-center justify-center min-h-screen from-teal-100 via-teal-300 to-teal-500 bg-gradient-to-br"
-          >
-            <div
-              class="w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl"
-            >
-              <div class="max-w-md mx-auto space-y-6">
-                <p class="text-gray-600">
-                  Quizz {{ trainingStore.trainingInfo.levels[1].title }}
-                </p>
+          <div class="flex">
+            <div class="w-full px-10 py-8 mx-auto">
+              <div class="mx-auto space-y-6 dark:text-white text-gray-600">
+                <div>
+                  <p>
+                    <span class="font-bold">Quizz</span>:
+                    {{ trainingStore.trainingInfo.levels[1].title }}
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <span class="font-bold">Formation</span>:
+                    {{ trainingStore.trainingInfo.title }}
+                  </p>
+                </div>
 
                 <div
                   v-for="quiz in trainingStore.trainingInfo.levels[1].quiz
@@ -38,7 +45,7 @@ const customElementsForm = reactive({
                   :key="quiz"
                   class="text-base leading-7"
                 >
-                  <p class="font-medium text-gray-700">
+                  <p class="font-medium">
                     {{ quiz.question }}
                   </p>
                   <FormField>
@@ -61,13 +68,13 @@ const customElementsForm = reactive({
                     >
                   </p> -->
                 </div>
-                <a
+                <!-- <a
                   target="_blank"
                   href="https://tailwindcomponents.com"
                   class="block w-full px-4 py-2 font-medium tracking-wide text-center text-white capitalize transition-colors duration-200 transform bg-teal-400 rounded-md hover:bg-teal-500 focus:outline-none focus:ring focus:ring-teal-300 focus:ring-opacity-80"
                 >
                   Go Back to Tailwind Components
-                </a>
+                </a> -->
               </div>
             </div>
           </div>
