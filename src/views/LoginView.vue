7b580b045b03a745b74afc6b5dbb5d45b311e3c2
toky-sowell <emar@sowellapp.com>
2023-09-05 23:31:48 +0300

    add validation, in login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,16 +8,69 @@ import BaseButton from "@/components/BaseButton.vue";
 import BaseButtons from "@/components/BaseButtons.vue";
 import LayoutGuest from "@/layouts/LayoutGuest.vue";
 import { useAuthStore } from "@/stores/auth";
+import * as yup from "yup";
+import { reactive, watch } from "vue";
 
 const auth = useAuthStore();
 
+const schema = yup.object().shape({
+  username: yup.string().required("Ce champ est requis"),
+  password: yup.string().required("Ce champ est requis"),
+});
+
+const validations = reactive({
+  username: {
+    fn: (value) =>
+      schema.validateAt("username", {
+        username: value,
+      }),
+    msg: "required",
+    error: "",
+  },
+  password: {
+    fn: (value) =>
+      schema.validateAt("password", {
+        password: value,
+      }),
+    msg: "required",
+    error: "",
+  },
+});
+
+const handleLogin = async () => {
+  try {
+    // Valider le champ "Login"
+    await validations.username.fn(auth.user.username);
+    await validations.password.fn(auth.user.password);
+    // Si la validation réussit, appeler auth.login
+    await auth.login();
+  } catch (error) {
+    validations.username.error = error.message;
+    validations.password.error = error.message;
+  }
+};
+
+const createValidationWatcher = (property, key) => {
+  return watch(
+    () => property,
+    (newValue) => {
+      console.log("niova e ");
+      if (newValue !== "") {
+        validations[key].error = "";
+      }
+    }
+  );
+};
+
+createValidationWatcher(auth.user.username, "username");
+createValidationWatcher(auth.user.password, "password");
 </script>
 
 <template>
   <LayoutGuest>
     <SectionFullScreen v-slot="{ cardClass }" bg="purplePink">
       <CardBox :class="cardClass">
-        <FormField label="Login" help="Please enter your login">
+        <FormField label="Login" :help="validations['username'].error">
           <FormControl
             v-model="auth.user.username"
             :icon="mdiAccount"
@@ -26,7 +79,7 @@ const auth = useAuthStore();
           />
         </FormField>
 
-        <FormField label="Password" help="Please enter your password">
+        <FormField label="Password" :help="validations['password'].error">
           <FormControl
             v-model="auth.user.password"
             :icon="mdiAsterisk"
@@ -35,21 +88,13 @@ const auth = useAuthStore();
             autocomplete="current-password"
           />
         </FormField>
-
-        <!-- <FormCheckRadio
-          v-model="form.remember"
-          name="remember"
-          label="Remember"
-          :input-value="true"
-        /> -->
-
         <template #footer>
           <BaseButtons>
             <BaseButton
               type="submit"
               color="info"
               label="Login"
-              @click="auth.login"
+              @click="handleLogin"
             />
             <BaseButton color="info" outline label="Signup" />
           </BaseButtons>
