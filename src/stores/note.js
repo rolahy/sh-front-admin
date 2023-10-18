524fbca564cba98c612fa43859c7756ba5d7bb9b
toky-sowell <emar@sowellapp.com>
2023-10-18 21:14:36 +0300

    feat add note store

---
+++ src/stores/note.js
@@ -0,0 +1,24 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const useAuthStore = defineStore("note", {
+  state: () => ({
+    note: {
+      userId: "",
+      trainingId: "",
+      note: 0,
+    },
+  }),
+  actions: {
+    login() {
+      axios
+        .post("https://sh-api-v1.vercel.app/notes", this.note)
+        .then((res) => {
+          console.log("notes", res);
+        })
+        .catch((error) => {
+          console.log("erreur", error);
+        });
+    },
+  },
+});
