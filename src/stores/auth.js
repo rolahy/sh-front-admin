7b333c5c3d850256187d432e603862d57f69daeb
toky-sowell <emar@sowellapp.com>
2023-10-18 23:45:25 +0300

    feat useCOnnected

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -7,6 +7,7 @@ export const useAuthStore = defineStore("auth", {
       username: "",
       password: "",
     },
+    userConnected: undefined,
     access_token: "",
     isLogin: false,
   }),
@@ -19,12 +20,24 @@ export const useAuthStore = defineStore("auth", {
           localStorage.setItem("access_token", res.data.access_token);
           this.access_token = res.data.access_token;
           this.router.push({ name: "dashboard" });
+          this.getUserconnected();
         })
         .catch((error) => {
           console.log("erreur", error);
         })
         .finally(() => {
           this.isLogin = false;
+        });
+    },
+    getUserconnected() {
+      axios
+        .post("https://sh-api-v1.vercel.app/auth/login", this.user)
+        .then((res) => {
+          console.log("use connected", res.data);
+          // localStorage.setItem("userConnected", res.data.access_token);
+        })
+        .catch((error) => {
+          console.log("erreur", error);
         });
     },
   },
