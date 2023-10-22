e21b58557f23bc61962e8b64e1d99493fd83676e
toky-sowell <emar@sowellapp.com>
2023-10-22 12:03:06 +0300

    undo app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <script setup>
 import { RouterView } from "vue-router";
-import { onMounted } from "vue";
-import { useTrainingStore } from "@/stores/training";
-
-const trainingStore = useTrainingStore();
-onMounted(() => {
-  if (localStorage.getItem("idTraining") != null) {
-    trainingStore.trainingInfo = localStorage.getItem("idTraining");
-    console.log(
-      "trainingStore.trainingInfo locale storage",
-      trainingStore.trainingInfo
-    );
-  }
-});
 </script>
 
 <template>
