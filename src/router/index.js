bc55ab033f86330934715e1898c61e5b29d68c8b
toky-sowell <emar@sowellapp.com>
2023-11-11 21:09:01 +0300

    log 1

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ const routes = [
     component: Home,
     beforeEnter: () => {
       const auth = useAuthStore();
-      console.log("yayayayaay", auth);
+      console.log("yayayayaay", auth.userConnected.value);
       // if (!auth.userConnected.roles[0].role.includes("super_admin")) {
       //   return next({ name: "board" });
       // }
